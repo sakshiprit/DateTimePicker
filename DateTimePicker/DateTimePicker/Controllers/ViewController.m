2165d56e514d2a8906b2191577b1b18800f3fa8c
Sakshi <sakshipatil165@gmail.com>
2024-02-15 08:30:30 +0100

    - added uiTextView to show the welcome message . - added viewmodel to the uiviewcontroller to provide data to the uiTextView - modifed the way we display views by seperating  them out in own functions for better readability.

--- DateTimePicker/DateTimePicker/Controllers/ViewController.m
+++ DateTimePicker/DateTimePicker/Controllers/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) UITextView *welcomeTextView;
+@property (strong, nonatomic) StartViewModel *viewModel;
+
 @end
 
 @implementation ViewController
@@ -17,7 +20,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Start button
+    self.viewModel = [[StartViewModel alloc] init];
+    
+    [self welcomeText];
+    
+    [self startButton];
+    
+    self.view.backgroundColor  = UIColor.blackColor;
+}
+
+/// Start button view
+- (void)startButton {
     UIButton *startButton = [UIButton buttonWithType:UIButtonTypeSystem];
     startButton.backgroundColor = UIColor.blueColor;
     startButton.frame = CGRectMake(100, 100, 100, 50);
@@ -29,9 +42,27 @@
     [self.view addSubview:startButton];
 }
 
+/// Welcome text view
+- (void)welcomeText {
+    self.welcomeTextView = [[UITextView alloc] initWithFrame:CGRectMake(20, self.view.center.y - 140, self.view.bounds.size.width - 40, 100)];
+    self.welcomeTextView.backgroundColor = [UIColor clearColor];
+    self.welcomeTextView.textAlignment = NSTextAlignmentCenter;
+    self.welcomeTextView.editable = NO;
+    self.welcomeTextView.font = [UIFont systemFontOfSize:24];
+    self.welcomeTextView.textColor = [UIColor whiteColor];
+    self.welcomeTextView.contentInset = UIEdgeInsetsMake(0, 0, 20, 0);
+    [self.view addSubview:self.welcomeTextView];
+}
+
+// Action to be performed when view appears.
+-(void)viewWillAppear:(BOOL)animated {
+    self.welcomeTextView.text = [self.viewModel getWelcomeMessageFor:@"Sakshi"];
+}
+
 // Presents DateTimePicker using `SwiftUIViewWrapper`
 - (void)presentSwiftUIView:(UIButton *)sender {
     UIViewController *swiftUIViewController = [SwiftUIViewWrapper createSwiftUIView];
+    swiftUIViewController.modalPresentationStyle = UIModalPresentationFullScreen;
     [self presentViewController:swiftUIViewController animated:YES completion:nil];
 }
 
